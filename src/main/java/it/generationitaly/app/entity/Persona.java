6b883b966036fddd12634deaeefda7c448e7e271
andreabruno <andreabruno@air-di-andrea.home>
2023-06-21 15:06:40 +0200

    add property id, codiceFiscale, nome, cognome to Persona.java, created PersonaRepository.java...

--- src/main/java/it/generationitaly/app/entity/Persona.java
+++ src/main/java/it/generationitaly/app/entity/Persona.java
@@ -2,8 +2,47 @@ package it.generationitaly.app.entity;
 
 public class Persona {
 
-	public Persona() {
-		// TODO Auto-generated constructor stub
+	private int id;
+	private String codiceFiscale;
+	private String nome;
+	private String cognome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCodiceFiscale() {
+		return codiceFiscale;
+	}
+
+	public void setCodiceFiscale(String codiceFiscale) {
+		this.codiceFiscale = codiceFiscale;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", codiceFiscale=" + codiceFiscale + ", nome=" + nome + ", cognome=" + cognome
+				+ "]";
 	}
 
 }
---
+++ src/main/java/it/generationitaly/app/repository/PersonaRepositoty.java
@@ -0,0 +1,8 @@
+package it.generationitaly.app.repository;
+
+import it.generationitaly.app.entity.Persona;
+
+public interface PersonaRepositoty {
+
+	void save (Persona persona);
+}
