3b93ee6479af5639b0db455b03b24dca98ac9a81
andreabruno <andreabruno@air-di-andrea.home>
2023-06-21 15:24:46 +0200

    create PersonaRepositoryImpl.java, create method save into PersonaRepositoryImpl.java,

---
+++ src/main/java/it/generationitaly/app/repository/impl/PersonaRepositoryImpl.java
@@ -0,0 +1,12 @@
+package it.generationitaly.app.repository.impl;
+
+import it.generationitaly.app.entity.Persona;
+import it.generationitaly.app.repository.PersonaRepositoty;
+
+public class PersonaRepositoryImpl implements PersonaRepositoty {
+
+	public void save(Persona persona) {
+		// TODO Auto-generated method stub
+	}
+
+}
