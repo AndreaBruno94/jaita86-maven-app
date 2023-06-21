5077d0f2b8a8bdd5194e5e9e4fe448ae4a1befa5
andreabruno <andreabruno@air-di-andrea.home>
2023-06-21 15:31:53 +0200

    create Utente.java, create crud UtenteRepository.java

---
+++ src/main/java/it/generationitaly/app/entity/Utente.java
@@ -0,0 +1,7 @@
+package it.generationitaly.app.entity;
+
+public class Utente {
+
+	
+
+}
--- src/main/java/it/generationitaly/app/repository/UtenteRepository.java
+++ src/main/java/it/generationitaly/app/repository/UtenteRepository.java
@@ -1,5 +1,16 @@
 package it.generationitaly.app.repository;
 
+import java.util.List;
+
 public interface UtenteRepository {
 
+	void save (Utente utente);
+	
+	void update (Utente utente);
+	
+	void delete (Utente utente);
+	
+	Utente findById (int id);
+	
+	List<Utente> findAll();
 }
